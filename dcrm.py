2865948a457049bd91a1d611411cb32af9af7cfb
Aleksi <aleksi.maki-penttila@tuni.fi>
2021-08-04 05:06:04 +0300

    fix args

--- dcrm.py
+++ dcrm.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser()
     parser.add_argument('mode', help='master/slave/server', type=str)
-    parser.add_argument('-hosts', help='host address(es) used for the slave machine or actor-/regret server', type=str, nargs='+')
+    parser.add_argument('-host', help='host address used for the slave machine', type=str)
+    parser.add_argument('-hosts', help='host address(es) used for the actor-/regret server(s)', type=str, nargs='+')
     args = parser.parse_args()
     options = [('grpc.max_send_message_length', -1), ('grpc.max_receive_message_length', -1)]
 
@@ -23,11 +24,11 @@ if __name__ == "__main__":
         logging.info("Master server serving at %s", MASTER_HOST)
         server.wait_for_termination()
     elif args.mode == 'slave':
-        if args.hosts[0] not in SLAVE_HOSTS:
+        if args.host not in SLAVE_HOSTS:
             raise ValueError("Unrecognized hostname for slave, check config file.")
         server = grpc.server(ThreadPoolExecutor(max_workers=2), options=options)
         RL_pb2_grpc.add_SlaveServicer_to_server(Slave(), server)
-        server.add_insecure_port(args.hosts[0])
+        server.add_insecure_port(args.host)
         server.start()
         logging.info("Slave server serving at %s", args.hosts[0])
         server.wait_for_termination()
